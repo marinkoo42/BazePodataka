d69e30ee2ba54483e0a69dd392ac779908bbdef6
Ivan Markovic <ivan.markovic@elfak.rs>
2022-12-26 20:13:15 +0100

    dodat client za neo4j

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
+using Neo4jClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var client = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "edukacija");
+client.ConnectAsync();
+builder.Services.AddSingleton<IGraphClient>(client);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
