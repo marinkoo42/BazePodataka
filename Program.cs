ab23a47eab0104449a7f0c183bb4eec7dbed0a13
marinkoo42 <marikoo@elfak.rs>
2022-12-26 20:31:42 +0100

    dodat redis client

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
 using Neo4jClient;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//neo4j 
 var client = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "edukacija");
 client.ConnectAsync();
 builder.Services.AddSingleton<IGraphClient>(client);
+
+//redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+                  ConnectionMultiplexer.Connect("localhost:6379"));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
 
 
 var app = builder.Build();
